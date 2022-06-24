0d1933a3d277a838960067d11c8afe44ed671019
SubhajitGhorai <107997027+SubhajitGhorai@users.noreply.github.com>
2022-06-24 11:39:32 +0530

    Add files via upload

---
+++ Multiplytwonumbers.java
@@ -0,0 +1,14 @@
+//java program to print Multiplication of two numbers
+class Multiplytwonumbers {
+
+   public static void main(String[] args) {
+        
+     //variables
+      int num1 = 5, num2 = 10, multiply;
+     //Multiplication
+       multiply = num1 * num2;
+//to print the Multiplication of a & b 
+
+      System.out.println("Multiply of these numbers:"+multiply);
+   }
+}
