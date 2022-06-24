d8141fee82c4330298c4a8a7444bca340121f61a
SubhajitGhorai <107997027+SubhajitGhorai@users.noreply.github.com>
2022-06-24 11:44:55 +0530

    Add files via upload

---
+++ Addtwonumbers.java
@@ -0,0 +1,13 @@
+//java program to print addition of two numbers
+   class Addtwonumbers {
+
+   public static void main(String[] args) {
+        
+     //variables
+      int num1 = 5, num2 = 15, sum;
+     //Addition
+      sum = num1 + num2;
+//to print the sum of a & b
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
